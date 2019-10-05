a9e45bf2d3ec1f9a0c4cb1ca59c55504c40c06e5
jt250054 <jt250054@teradata.com>
2019-10-06 01:32:09 +0530

    Update timer

--- irctc.py
+++ irctc.py
@@ -10,14 +10,14 @@ username="g1rao18"
 password="ghg756"
 origin="PALASA - PSA"
 destination="SECUNDERABAD JN - SC"
-_date="29-09-2019"
+_date="01-10-2019"
 name = 'Jeevan Rao T'
 age = '24'
 
 browser = webdriver.Chrome(r"C:\Users\jt250054\Documents\chromedriver_win32\chromedriver.exe")
 
 def waitFor(x_path=None,id=None):
-        timeout = 15
+        timeout = 60
         wait = ui.WebDriverWait(browser,timeout)
         if x_path:
                 wait.until(lambda browser: browser.find_element_by_xpath(x_path))
@@ -46,24 +46,30 @@ def search_train(browser,origin,destination,_date):
         #waitFor(origin_xpath)
         origin_object = browser.find_element_by_xpath(origin_xpath)
         origin_object.send_keys(origin)
+        time.sleep(1)
         destination_object = browser.find_element_by_xpath('//*[@id="destination"]/span/input')
         destination_object.send_keys(destination)
+        time.sleep(1)
         date_object = browser.find_element_by_xpath('//*[@id="divMain"]/div/app-main-page/div[1]/div/div[1]/div/div/div[1]/div/app-jp-input/div[3]/form/div[3]/p-calendar/span/input')            
         [date_object.send_keys(Keys.BACKSPACE) for _ in range(10)]
-        date_object.send_keys(_date)        
+        date_object.send_keys(_date)
+        time.sleep(1)        
         browser.find_element_by_xpath('//*[@id="divMain"]/div/app-main-page/div[1]/div/div[1]/div/div/div[1]/div/app-jp-input/div[3]/form/div[7]/button').submit()
 
         tatkal_lnk_object = '//*[@id="divMain"]/div/app-train-list/div/div[5]/div/div[2]/div[1]/div[2]/div[2]/div/div[3]/div/div[2]/p-dropdown/div/label'
         waitFor(tatkal_lnk_object)
         tatkal_object = '//*[@id="divMain"]/div/app-train-list/div/div[5]/div/div[2]/div[1]/div[2]/div[2]/div/div[3]/div/div[2]/p-dropdown/div/div[4]/div/ul/li[5]/span'
         browser.find_element_by_xpath(tatkal_lnk_object).click()
+        time.sleep(1)
         browser.find_element_by_xpath(tatkal_object).click()
+        time.sleep(1)
         falak_train = '//*[@id="check-availability"]'
         waitFor(falak_train)
         browser.find_element_by_id('check-availability').click()
         #_________________BookNowCode to be added_____________________________#
 
 def fill_details(browser,name,age):
+        waitFor(None,"psgn-name")
         name_object = browser.find_element_by_id("psgn-name")
         name_object.send_keys(name)
         age_object = browser.find_element_by_xpath('//*[@id="divMain"]/div/app-passenger-input/div[5]/form/div/div[1]/div[3]/div[1]/div/div[2]/app-passenger/div/div[1]/div[2]/input')
