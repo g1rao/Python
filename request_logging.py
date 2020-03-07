952a9ecc15f7f61f290a1284991aa942384ea388
Jeevan Rao <jeevanrao.iiit@gmail.com>
2020-03-07 12:34:32 +0530

    Update request_logging.py

--- request_logging.py
+++ request_logging.py
@@ -1,3 +1,9 @@
+import threading
+import logging
+from django.utils.deprecation import MiddlewareMixin
+
+local = threading.local()
+
 class CustomLoggingMiddleware(MiddlewareMixin):
     def process_request(self, request):
         remote_addr = request.META['REMOTE_ADDR']
@@ -7,3 +13,6 @@ class CustomLoggingFilter(logging.Filter):
     def filter(self, record):        
         record.remote_addr = getattr(local, 'remote_addr', 'no host')
         return True
+
+# Configure filters 
+LOCAL_SETTINGS = ""
